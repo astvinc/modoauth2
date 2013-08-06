a138ae49536c73983e46c65e7934b43504809ca7
Vanessa Ramirez <vramirez@astvinc.com>
2013-08-06 13:48:37 -0400

    Fixing server paths in token and resource snippet

--- core/components/modoauth/elements/snippets/snippet.resource.php
+++ core/components/modoauth/elements/snippets/snippet.resource.php
@@ -1,6 +1,9 @@
 <?php
 // include our OAuth2 Server object
-require_once $modx->getOption('modoauth.core_path',null,$defaultCorePath.'components/oauth2/').'elements/snippets/snippet.server.php';
+$defaultCorePath = $modx->getOption('core_path').'components/modoauth/';
+$modaouthCorePath = $modx->getOption('modoauth.core_path',null,$defaultCorePath);
+
+require_once $modaouthCorePath.'elements/snippets/snippet.server.php';
 
 // Handle a request for an OAuth2.0 Access Token and send the response to the client
 if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
